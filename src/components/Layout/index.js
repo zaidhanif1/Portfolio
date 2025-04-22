9488fe5ca9cdf559cace600351ccdf3c32cbd306
zaidhanif1 <zah00005@mix.wvu.edu>
2025-04-22 09:44:32 -0400

    First commit

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,23 +1,13 @@
-import { Outlet } from 'react-router-dom'
-import Sidebar from '../Sidebar/'
-import './index.scss'
-
-const Layout = () => {
+import React from 'react';
+import './index.scss';
+import Sidebar from '../Sidebar';
+const Layout = () => 
+{
   return (
-    <div className="App">
-      <Sidebar />
-      <div className="page">
-        <span className="tags top-tags">&lt;body&gt;</span>
+  <div>
+    <Sidebar />
 
-        <Outlet />
-        <span className="tags bottom-tags">
-          &lt;/body&gt;
-          <br />
-          <span className="bottom-tag-html">&lt;/html&gt;</span>
-        </span>
-      </div>
-    </div>
-  )
-}
-
+  </div>
+  );
+};
 export default Layout
